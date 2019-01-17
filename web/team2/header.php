b7187aff55cb8c15b51f5ec55baa8318706c303d
Devi Damsel <scstolp@gmail.com>
2019-01-17 12:41:14 -0700

    fixed i think

--- web/team2/header.php
+++ web/team2/header.php
@@ -4,16 +4,16 @@
 </head>
 <body>
 <?php 
-    $file = $_GET['file'];
+    $file = $_SERVER['HTTP_REFERER'];
     echo "the file is: $file";
-    if ($file == "home") {
+    if ($file == "https://vast-cliffs-71677.herokuapp.com/team2/home/php") {
     echo "<ul>
         <p><a style='font-weight:bold' href='./home.php'>Home</a></p>
         <p><a href='./about.php'>About us</a></p>
         <p><a id='login' href='./login.php'>Log In</a></p>
         </ul>
         </div>";
-    } else if ($file == "about") {
+    } else if ($file == "https://vast-cliffs-71677.herokuapp.com/team2/about.php") {
        echo "<div>
     <ul>
         <p><a href='./home.php'>Home</a></p>
