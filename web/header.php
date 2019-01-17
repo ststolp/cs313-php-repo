63801941da78212d35f3db78c6ac72caff1ce17c
Devi Damsel <scstolp@gmail.com>
2019-01-17 11:30:21 -0700

    some header

--- web/header.php
+++ web/header.php
@@ -8,26 +8,26 @@
     if ($file == "home") {
       echo "<div>
     <ul>
-        <a style='font-weight:bold' id='home' href='./home.php'>Home</a>
-        <a id='about' href='./about.php'>About us</a>
-        <a id='login' href='./login.php'>Log In</a>
+        <p><a style='font-weight:bold' id='home' href='./home.php'>Home</a></p>
+        <p><a id='about' href='./about.php'>About us</a></p>
+        <p><a id='login' href='./login.php'>Log In</a></p>
         </ul>
         </div>";
     } else if ($file == "about") {
        echo "<div>
     <ul>
-        <a id='home' href='./home.php'>Home</a>
-        <a style='font-weight: bold' id='about' href='./about.php'>About us</a>
-        <a id='login' href='./login.php'>Log In</a>
+        <p><a id='home' href='./home.php'>Home</a></p>
+        <p><a style='font-weight: bold' id='about' href='./about.php'>About us</a></p>
+        <p><a id='login' href='./login.php'>Log In</a></p>
         </ul>
         </div>";
     }
     else {
       echo "<div>
     <ul>
-        <a id='home' href='./home.php'>Home</a>
-        <a id='about' href='./about.php'>About us</a>
-        <a style='font-weight:bold' id='login' href='./login.php'>Log In</a>
+        <p><a id='home' href='./home.php'>Home</a></p>
+        <p><a id='about' href='./about.php'>About us</a></p>
+        <p><a style='font-weight:bold' id='login' href='./login.php'>Log In</a></p>
         </ul>
         </div>"
     }
