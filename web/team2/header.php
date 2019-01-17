8137883a0f8a21b70f444a8f813c9888779968e0
Devi Damsel <scstolp@gmail.com>
2019-01-17 11:58:35 -0700

    changing

--- web/team2/header.php
+++ web/team2/header.php
@@ -4,15 +4,33 @@
 </head>
 <body>
 <?php 
-  
-?>
-
-<div>
+    $file = $_GET['file'];
+    if ($file == "home") {
+      echo "<p>The file is: $file</p><div>
     <ul>
-        <a href="./home.php">Home</a>
-        <a href="./about.php">About us</a>
-        <a href="./login.php">Log In</a>
+        <p><a style='font-weight:bold' href='./home.php'>Home</a></p>
+        <p><a href='./about.php'>About us</a></p>
+        <p><a id='login' href='./team2/login.php'>Log In</a></p>
         </ul>
-        </div>
+        </div>";
+    } else if ($file == "about") {
+       echo "<div>
+    <ul>
+        <p><a href='./home.php'>Home</a></p>
+        <p><a style='font-weight: bold' href='./team2/about.php'>About us</a></p>
+        <p><a href='./login.php'>Log In</a></p>
+        </ul>
+        </div>";
+    }
+    else {
+      echo "<div>
+    <ul>
+        <p><a href='./home.php'>Home</a></p>
+        <p><a href='./about.php'>About us</a></p>
+        <p><a style='font-weight:bold' href='./team2/login.php'>Log In</a></p>
+        </ul>
+        </div>"
+    }
+?>
 </body>
 </html>
