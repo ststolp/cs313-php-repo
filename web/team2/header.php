894fbad16af677eeea99203033de644b5696d8dd
Devi Damsel <scstolp@gmail.com>
2019-01-17 12:45:11 -0700

    fixed i think

--- web/team2/header.php
+++ web/team2/header.php
@@ -4,15 +4,15 @@
 </head>
 <body>
 <?php 
-    $file = $_SERVER['HTTP_REFERER'];
-    if ($file == "https://vast-cliffs-71677.herokuapp.com/team2/home.php") {
+    $file = $_SERVER['PHP_SELF'];
+    if ($file == "home.php") {
     echo "<ul>
         <p><a style='font-weight:bold' href='./home.php'>Home</a></p>
         <p><a href='./about.php'>About us</a></p>
         <p><a id='login' href='./login.php'>Log In</a></p>
         </ul>
         </div>";
-    } else if ($file == "https://vast-cliffs-71677.herokuapp.com/team2/about.php") {
+    } else if ($file == "about.php") {
        echo "<div>
     <ul>
         <p><a href='./home.php'>Home</a></p>
