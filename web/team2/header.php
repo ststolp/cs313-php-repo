dd3e45ea9c12fe4cb9fdb4e588e48c0a38f49e96
Devi Damsel <scstolp@gmail.com>
2019-01-17 12:47:25 -0700

    fixed i think

--- web/team2/header.php
+++ web/team2/header.php
@@ -6,14 +6,14 @@
 <?php 
     $file = $_SERVER['PHP_SELF'];
     echo "The file name is $file";
-    if ($file == "home.php") {
+    if ($file == "/team2/home.php") {
     echo "<ul>
         <p><a style='font-weight:bold' href='./home.php'>Home</a></p>
         <p><a href='./about.php'>About us</a></p>
         <p><a id='login' href='./login.php'>Log In</a></p>
         </ul>
         </div>";
-    } else if ($file == "about.php") {
+    } else if ($file == "/team2/about.php") {
        echo "<div>
     <ul>
         <p><a href='./home.php'>Home</a></p>
