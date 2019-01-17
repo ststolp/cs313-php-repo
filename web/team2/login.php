81c929da61e6f59a948c67e2e2a45293cbfd5df7
Devi Damsel <scstolp@gmail.com>
2019-01-17 13:27:33 -0700

    changing style

--- web/team2/login.php
+++ web/team2/login.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
+   <script>
+    $(document).ready(function(){
+       $("#admin").click(function() {
+          $("#vid").load("./logtype.php");
+       });
+   });
+    $(document).ready(function(){
+       $("#tester").click(function() {
+          $("#vid").load("./logtype.php");
+       });
+   });
+   }
 </head>
 <body>
 <?php 
    include './header.php';
 ?>
-<button>Log in as an Administrator</button>
-<button>Log in as a Tester</button>
+<button id="admin">Log in as an Administrator</button>
+<button id="tester">Log in as a Tester</button>
 </body>
 </html>
---
+++ web/team2/logtype.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+<?php 
+  $logintype = $_GET["type"];
+ echo "You are logged in as $logintype";
+ ?>
+</body>
+</html>
