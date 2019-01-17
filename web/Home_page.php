c7a17a411a63b62067d8bb2ebe49332a36aa9267
Devi Damsel <scstolp@gmail.com>
2019-01-16 17:21:49 -0700

    Adding stylesheet, php, and images

--- web/Home_page.php
+++ web/Home_page.php
@@ -2,10 +2,53 @@
 <html>
 <head>
 <title>Steven's Home</title>
+<link rel="stylesheet" type="text/css" href="./home_css.css">
 </head>
 <body>
 <?php
-   
+   $timeofday = gettimeofday();
+   echo "The time of day is: $timeofday";
 ?>
+
+<h1>Talents of Steven Stolp</h1>
+<a href="./assignments.php">My assignments</a>
+<p>Hello, my name is Steven Stolp,
+ I'm from Utah and I'm the eighth child of nine children. I like to sing, draw, and write stories.
+ Below are some of my doodles I made on my phone. I would tell you a story but I decided to give my
+ crash course on creative writing.</p>
+ <div class="gallery">
+    <img src="./red.jpg" alt="red">
+       <div class="desc">Black and White Cartoon</div>
+</div> 
+
+<div class="gallery">
+   <img src="./house.jpg" alt="house">
+       <div class="desc">House</div>
+</div>
+
+<div class="gallery">
+    <img src="./girl-jumping.jpg" alt="jump">
+       <div class="desc">Girl Jumping</div>
+</div>
+
+<div class="gallery">
+    <img src="./sunset.jpg" alt="sunset">
+       <div class="desc">Sunset on an ocean</div>
+</div>
+
+<div class="gallery">
+    <img src="./dnd_rose.jpg" alt="dnd character">
+       <div class="desc">My D&D character</div>
+</div>
+
+ <div>
+ <h2>Creative Writing 101</h2>
+ <p>There are four essential elements a plot needs to be successful. The mroe closely you follow this
+ pattern, the more likely your plot will work. The elements are: Lead, Objective, Conflict, and Knockout, or
+ LOCK. The lead means that you have either a setting, main character, or situation that is interesting and
+ the reader wants to know more about him. Something out of the ordinary. There also needs to be an objective,
+ what does the character want? The stakes should also be high. Conflict means that something needs to say
+ no. Something or someone needs to stop the hero from reaching his goal. The story ends with a knockout.
+ This is how the main character resolves the conflict and he may or may not achieve his goal. 
 </body>
 </html>
---
+++ web/assignments.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CS313 Assignments</title>
+        <link rel="stylesheet" type="text/css" href="./home_css.css">
+</head>
+<body>
+    <h1>Coming Soon</h1>
+</body>
+</html>
