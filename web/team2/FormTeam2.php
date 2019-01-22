bf0141c4943b185f2349e705e398dea22bef590c
Devi Damsel <scstolp@gmail.com>
2019-01-22 11:48:18 -0700

    changed checkboxes

---
+++ web/team2/FormTeam2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+<form action="results.php" method="post">
+    Name: <input type="text" name="name">
+    Email: <input type="text"  name="email">
+    <p> Choose major</p>
+    <?php
+      $majors = ["CS" => "Computer Science", "WDD"=>"Web Design and Development",
+                 "CIT"=>"Computer Information Technology", "CE"=>"Computer Engineering"];
+        foreach($majors as $major) {
+            echo "<input type='radio' name ='major' value='$major'>$major<br>";
+        }
+   ?>
+    <input type="radio" name="major" value="Computer Science">Computer Science<br>
+    <input type="radio" name="major" value="Web Design and Development">Web design<br>
+    <input type="radio" name="major" value="CIT">CIT<br>
+    <input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
+    <p>Comments</p>
+    <textarea name="comments"></textarea>   
+    <input type="checkbox" name="check[]" value="NA">North America<br>
+    <input type="checkbox" name="check[]" value="SA">South America<br>
+    <input type="checkbox" name="check[]" value="Europe">Europe<br>
+    <input type="checkbox" name="check[]" value="Asia">Asia<br>
+    <input type="checkbox" name="check[]" value="Australia">Australia<br>
+    <input type="checkbox" name="check[]" value="Africa">Africa<br>
+    <input type="checkbox" name="check[]" value="Antarctica">Antarctica<br>
+    <input type="submit">
+</form>
+</body>
+</html>
