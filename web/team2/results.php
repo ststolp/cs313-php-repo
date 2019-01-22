45137487a489bfa41d24ef34b8ce6701042a0183
Devi Damsel <scstolp@gmail.com>
2019-01-22 10:25:00 -0700

    form

---
+++ web/team2/results.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+<?php
+    echo "Your name is: $_GET['name'];
+    <br> your email is: $_GET['email'];
+    <br> Your major is: $_GET['major'];
+    <br> Your comments: $_GET['comments'];"
+    ?>
+</body>
+</html>
