9b887a020ab71cdbef9a6cb79e66a4b90783de33
Devi Damsel <scstolp@gmail.com>
2019-01-22 10:33:49 -0700

    fixed

--- web/team2/results.php
+++ web/team2/results.php
@@ -3,10 +3,10 @@
 </head>
 <body>
 <?php
-    echo "Your name is: $_GET['name'];
-    <br> your email is: $_GET['email'];
-    <br> Your major is: $_GET['major'];
-    <br> Your comments: $_GET['comments'];"
+    echo "Your name is: $_GET["name"];
+    <br> your email is: $_GET["email"];
+    <br> Your major is: $_GET["major"];
+    <br> Your comments: $_GET["comments"];"
     ?>
 </body>
 </html>
