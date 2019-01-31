8ac628433803db463e0e9712a99c74e4d08a201f
Devi Damsel <scstolp@gmail.com>
2019-01-31 09:18:41 -0700

    made many books for one patron instead of many patrons for one book

--- db/myDb.sql
+++ db/myDb.sql
@@ -10,20 +10,23 @@ CREATE TABLE author (
 	FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
 );
 
+CREATE TABLE patron (
+	patron_id  int PRIMARY KEY,
+	fname VARCHAR(255),
+    lname VARCHAR(255)
+);
+
 CREATE TABLE books (
 	book_id int PRIMARY KEY,
 	title VARCHAR(255),
 	author_id int,
 	genre_id int,
+    patron_id int,
 	year DATE,
 	publisher VARCHAR(255),
 	FOREIGN KEY (author_id) REFERENCES author(author_id),
-	FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+	FOREIGN KEY (genre_id) REFERENCES genre(genre_id),
+    FOREIGN KEY (patron_id) REFERENCES patron(patron_id)
 );
 
-CREATE TABLE patron (
-	patron_id  int PRIMARY KEY,
-	name VARCHAR(255),
-	book_id int,
-	FOREIGN KEY (book_id) REFERENCES books(book_id)
-);
+
