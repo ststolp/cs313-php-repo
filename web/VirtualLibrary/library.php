83e814cf83c2819cf108eacc243daa5aedf85b05
Devi Damsel <scstolp@gmail.com>
2019-02-09 11:16:59 -0700

    lib

--- web/VirtualLibrary/library.php
+++ web/VirtualLibrary/library.php
@@ -5,15 +5,9 @@ sesstion_start();
 <head>
 </head>
 <body>
-<h1>The Library</h1><br>
+<h1>The Library</h1>
 <?php
 $db = $_SESSION['db'];
-/*foreach ($db->query('SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b 
-   INNER JOIN author a ON b.author_id = a.author_id') as $row) 
-   {
-       echo '<p><b>' . $row['b.title'] . ' by ' . $row['a.fname'] . ' ' . $row['a.lname'] . 
-       ' ' . $row['b.publisher'] . ', ' . $row['b.year'] . '</p>';
-   }*/
    $statement = $db->query('SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b 
    INNER JOIN author a ON b.author_id = a.author_id');
    while ($row = $statement->fetch(PDO:FETCH_ASSOC))
