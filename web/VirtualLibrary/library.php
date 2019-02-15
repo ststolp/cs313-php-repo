f3aff6aab6c4e6ccad7a63f01b1f4d00e75a13d4
Devi Damsel <scstolp@gmail.com>
2019-02-15 16:09:47 -0700

    fixing

--- web/VirtualLibrary/library.php
+++ web/VirtualLibrary/library.php
@@ -1,6 +1,29 @@
 <?php
-require_once("../dbConnect.php");
-$db = get_db();
+//require_once("../dbConnect.php");
+$db = NULL;
+	try {
+		$dbUrl = getenv('DATABASE_URL');
+		if (!isset($dbUrl) || empty($dbUrl)) {
+
+		}
+		// Get the various parts of the DB Connection from the URL
+		$dbopts = parse_url($dbUrl);
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+		// If this were in production, you would not want to echo
+		// the details of the exception.
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
+	$db = get_db();
 ?>
 <!DOCTYPE html>
 <head>
