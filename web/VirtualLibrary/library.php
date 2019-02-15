30564d075435b1afdd7a6e3602592275b46f4319
Devi Damsel <scstolp@gmail.com>
2019-02-15 15:52:47 -0700

    Added colon

--- web/VirtualLibrary/library.php
+++ web/VirtualLibrary/library.php
@@ -14,8 +14,8 @@ try
    $statement = $db->prepare('SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b 
    INNER JOIN author a ON b.author_id = a.author_id');
    $statement->execute();
-   
-   while ($row = $statement->fetch(PDO:FETCH_ASSOC))
+
+   while ($row = $statement->fetch(PDO::FETCH_ASSOC))
    {
       echo '<p><b>' . $row['b.title'] . ' by ' . $row['a.fname'] . ' ' . $row['a.lname'] . 
        ' ' . $row['b.publisher'] . ', ' . $row['b.year'] . '</p>'; 
