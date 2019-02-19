d357d77080e394d928f144970dbe67e71eaf58f5
Devi Damsel <scstolp@gmail.com>
2019-02-19 14:38:08 -0700

    create user

--- web/VirtualLibrary/create_user.php
+++ web/VirtualLibrary/create_user.php
@@ -11,8 +11,8 @@ $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 $query = 'INSERT INTO user (username, password)
         VALUES (:username, :password)';
 $statement = $db->prepare($query);
-$statement->bindValue(':username', $username, PDO::PARAM_INT));
-$statement->bindValue(':password', $hashed_password, PDO::PARAM_INT));
+$statement->bindValue(':username', $username, PDO::PARAM_INT);
+$statement->bindValue(':password', $hashed_password, PDO::PARAM_INT);
 $db->execute();
 
 header("location: sign_in.php");
