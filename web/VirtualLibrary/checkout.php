e26739bc3ce26568ab0a98d7a94fd22a8e7b0acb
Devi Damsel <scstolp@gmail.com>
2019-02-20 11:37:23 -0700

    changing session

--- web/VirtualLibrary/checkout.php
+++ web/VirtualLibrary/checkout.php
@@ -17,15 +17,16 @@ if ($username == "") {
     <link rel="stylesheet" type="text/css" href="lib_style.css">
 </head>
 <body>
-<?php include 'header.php' ?>
+<?php include 'header.php'
+$checkout = $_POST['checkout'];
+$items_amount = count($checkout); ?>
     <p>Items will be available until the date 
     listed below.</p>
     <h3><?php echo "$items_amount"; ?> items checked out</h3>
     <p>Title______________________Due</p>
 <?php
 
-$checkout = $_POST['checkout'];
-$items_amount = count($checkout);
+
 
 $query = "SELECT patron_id FROM patron WHERE username = '$username'";
 $statement = $db->prepare($query);
@@ -37,15 +38,21 @@ $_SESSION['user_id'] = $user_id;
   $query_date = "SELECT CURRENT_DATE";
     $statement_date = $db->prepare($query_date);
     $statement_date->execute();
-    $current_date = $statement_date->fetch(PDO::FETCH_ASSOC);
+    $current_date_array = $statement_date->fetch(PDO::FETCH_ASSOC);
+   $current_date = $current_date_array['CURRENT_DATE'];
 
-     $query_due = "SELECT CURRENT_DATE";
+     $query_due = "SELECT CURRENT_DATE + interval '30' day";
     $statement_due = $db->prepare($query_due);
     $statement_due->execute();
-    $due_date = $statement_due->fetch(PDO::FETCH_ASSOC);
-
+    $due_date_array = $statement_due->fetch(PDO::FETCH_ASSOC);
+     $due_date = $due_date_array["CURRENT_DATE + integer '30' day"];
 foreach($checkout as $newBook) {
-    echo "<p>$newBook _________________ $due_date</p>";
+    $query = "SELECT title FROM books WHERE book_id = '$newBook'";
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $title_array = $statement->fetch(PDO::FETCH_ASSOC);
+    $title_of_book = $title_array['title'];
+    echo "<p>$title_of_book _________________ $due_date</p>";
 
 }
 echo "<p>$current_date</p>";
