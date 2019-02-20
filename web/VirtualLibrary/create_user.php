9650a6ee38c5084f6d8dcb4eade28e8b067aae47
Devi Damsel <scstolp@gmail.com>
2019-02-20 12:52:15 -0700

    check confirm

--- web/VirtualLibrary/create_user.php
+++ web/VirtualLibrary/create_user.php
@@ -10,7 +10,7 @@ $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 if ($password != $confirm_password) {
         header("Location: Register.php");
-}
+} else {
 $query = 'INSERT INTO patron (username, password)
         VALUES (:username, :password)';
 $statement = $db->prepare($query);
@@ -19,5 +19,6 @@ $statement->bindValue(':password', $hashed_password);
 $statement->execute();
 
 header("Location: sign_in.php");
+}
 die();
 ?>
