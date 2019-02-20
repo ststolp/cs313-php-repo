e46700de568043ec843d64906564002e5dcbff47
Devi Damsel <scstolp@gmail.com>
2019-02-20 11:25:48 -0700

    changing session

--- web/VirtualLibrary/my_books.php
+++ web/VirtualLibrary/my_books.php
@@ -15,15 +15,22 @@ if ($username == "") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="lib_style.css">
 </head>
 <body>
     <?php
     include 'header.php';
     echo"<h2>Your Books</h2>";
+  $query_patron_id = "SELECT patron_id FROM patron WHERE username = '$username'";
+  $statement_id = $db->prepare($query_patron_id);
+  $statement_id->execute();
+  $patron_id_array = $statement_id->fetch(PDO::FETCH_ASSOC);
+  $patron_id = $patron_id_array['patron_id']; 
+
    $query = "SELECT b.title, bp.due_date, bp.checked_out FROM books b
     INNER JOIN book_patron bp ON b.book_id = bp.book_id
     INNER JOIN patron p ON p.patron_id = bp.patron_id
-    WHERE bp.patron_id = $user_id";
+    WHERE bp.patron_id = $patron_id";
     $statement = $db->prepare($query);
     $statement->execute();
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
