72e398674dc820d96e20d289fb6f878090469faa
Devi Damsel <scstolp@gmail.com>
2019-02-25 15:29:05 -0700

    books

--- web/VirtualLibrary/contents.php
+++ web/VirtualLibrary/contents.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="lib_style.css">
 </head>
 <body>
     <?php include 'header.php'; ?>
